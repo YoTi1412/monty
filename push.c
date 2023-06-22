e48bb2ebe10174f89118f74e97faf07fd72a3d96
YoTi <yasserrafai5@gmail.com>
2023-06-22 18:51:28 +0100

    push and pall

--- push.c
+++ push.c
@@ -1,37 +1,51 @@
 #include "monty.h"
+
+/**
+ * is_integer - checks if a string is a valid integer
+ * @str: input string
+ * Return: 1 if string is a valid integer, 0 otherwise
+ */
+int is_integer(const char *str)
+{
+    if (str == NULL || *str == '\0')
+        return 0;
+
+    if (*str == '-' || *str == '+')
+        str++;
+
+    if (*str == '\0')
+        return 0;
+
+    while (*str != '\0')
+    {
+        if (*str < '0' || *str > '9')
+            return 0;
+        str++;
+    }
+
+    return 1;
+}
+
 /**
  * f_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
-*/
+ */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+    if (!bus.arg || !is_integer(bus.arg))
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+    int n = atoi(bus.arg);
+    if (bus.lifi == 0)
+        addnode(head, n);
+    else
+        addqueue(head, n);
 }
