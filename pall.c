0c129d7243f4c702f51e0292bfc60a4421454454
YoTi <yasserrafai5@gmail.com>
2023-06-22 18:52:56 +0100

    push and pall

--- pall.c
+++ pall.c
@@ -1,20 +1,21 @@
 #include "monty.h"
-
 /**
  * f_pall - prints the stack
  * @head: stack head
- * @counter: unused parameter
+ * @counter: no used
  * Return: no return
  */
 void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *current = *head;
-
+	stack_t *h;
 	(void)counter;
 
-	while (current != NULL)
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 }
